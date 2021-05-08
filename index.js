781d22ba81750f12ad8491aacfd65aedff1437bf
Laurice Howard <lauricehoward@Laurices-MacBook-Air.local>
2021-05-08 18:55:05 -0400

    This is the first phase of my portfolio for the ennd of Goldd of Gold Badge

--- index.js
+++
@@ -1,8 +0,0 @@
-/* Project links for my cards 
-CSS Creature: https://codepen.io/lhoward42/pen/gOgaMKe
-Star Wars Static Website: https://lhoward42.github.io/Static_Website/
-Animal Facts Generator API: https://lhoward42.github.io/apiChallenge1/
-Github Profile for Footer: https://github.com/lhoward42 
-*/
-
-
